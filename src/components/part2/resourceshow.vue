193881beee0f8e2d43fc7fbd5b91be28da35f4ea
yyz1996 <940937734@qq.com>
2020-06-20 19:24:25 +0800

    叶杨展修改前端

---
+++ src/components/part2/resourceshow.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "resourceshow"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/part2/space_granularity.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "space_granularity"
+    }
+</script>
+
+<style scoped>
+
+</style>
