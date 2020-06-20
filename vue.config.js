4e681dcec69cc9b908337bc0ab8296380f7fb59d
yyz1996 <940937734@qq.com>
2020-06-20 20:01:40 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,23 @@ module.exports = {
                 pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
                     '^/api': ''
                 }
-            }
+            },
+            '/lips': {
+                target: 'http://api.map.baidu.com', //API服务器的地址
+                ws: true,  //代理websockets
+                changeOrigin: true, // 虚拟的站点需要更管origin
+                pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
+                    '^/lips': ''
+                }
+            },
+            '/moc': {
+                target: 'https://www.fastmock.site/mock/7433813b1c79b90e88d0541ef0e6767d/vueProject', //API服务器的地址
+                ws: true,  //代理websockets
+                changeOrigin: true, // 虚拟的站点需要更管origin
+                pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
+                    '^/moc': ''
+                }
+            },
         },
     },
     // lintOnSave: false // 取消 eslint 验证
