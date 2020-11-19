fb13b1d3423c2e90ae18b23c0b38793886fc8883
Ryan <cn_ryan@outlook.com>
2020-11-19 21:00:28 +0800

    Add search input and adjust header span.

--- src/components/part3/Common/header.vue
+++ src/components/part3/Common/header.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="18">
+            <el-col :xs="8" :sm="10" :md="12" :lg="14" :xl="16">
                 <el-menu :router="true" class="el-menu" mode="horizontal"
                  @select="handleSelect"
                  background-color="#00659B"
@@ -20,7 +20,7 @@
                 </el-menu>
 
             </el-col>
-            <el-col :span="6">
+            <el-col :xs="16" :sm="14" :md="12" :lg="10" :xl="8">
             <el-menu :router="true"
                      class="el-menu"
                      mode="horizontal"
@@ -52,6 +52,17 @@
 <!--                        选项3<el-badge class="mark" :value="1" />-->
 <!--                    </el-menu-item>-->
 <!--                </el-submenu>-->
+              <el-menu-item>
+                <el-autocomplete
+                    id="search-input"
+                    :fetch-suggestions="querySearch"
+                    v-model="state"
+                    placeholder="请输入模块名称"
+                    @select="handleInputSelect"
+                    @keyup.enter.native="handleEnter">
+                  <i slot="suffix" class="el-input__icon el-icon-search" @click="handleEnter"></i>
+                </el-autocomplete>
+              </el-menu-item>
                 <el-menu-item index="/trade/Dashboard">工作台</el-menu-item>
                 <el-menu-item index="/console/index">个人中心</el-menu-item>
                 <el-menu-item index="/auction">拍卖中心</el-menu-item>
@@ -82,18 +93,65 @@
         name: "Header",
         data() {
             return {
+                components: [],
                 logoimg: logoimg,
+                state:'',
                 userimg: userprofile
             }
         },
         methods: {
+            handleEnter () {
+              let results = this.components.filter(this.createFilter(this.state));
+              console.log(results);
+              if(results.length !== 0) {
+                this.$router.push(results[0].index);
+                this.state = '';
+                return;
+              }
+              alert("没有找到相关的模块。请尝试重新搜索！");
+              this.state = '';
+            },
+            handleInputSelect(component) {
+              this.$router.push(component.index);
+              this.state = '';
+            },
             handleSelect(key, keyPath) {
                 console.log(key, keyPath);
             },
             logOut(){
                 removeToken();
                 this.$router.push("/login")
+            },
+            querySearch(queryString, cb) {
+              let components = this.components;
+              let results = queryString ? components.filter(this.createFilter(queryString)) : components;
+              cb(results);
+            },
+            createFilter(queryString) {
+              return (components) => {
+                return (components.value.toLowerCase().indexOf(queryString.toLowerCase()) !== -1);
+              }
+            },
+            loadAll() {
+              return [
+                {"value":"地图功能","index":"/trade/transactionProject/map"},
+                {"value":"联盟形成","index":"/trade/transactionProject/userTaskInput"},
+                {"value":"价格波动风险演化", "index":"/trade/riskPrediction/riskPage"},
+                {"value":"可用资源展示", "index":"/trade/Multimodal-multigranularity/resourceShow"},
+                {"value":"空间粒度", "index":"/trade/Multimodal-multigranularity/spaceGranularity"},
+                {"value":"在线与离线模态","index":"/trade/Multimodal-multigranularity/taskQuery"},
+                {"value":"人机资源管理","index":"/trade/Multimodal-multigranularity/modalityQuery"},
+                {"value":"主动与被动模态","index":"/trade/Multimodal-multigranularity/acpassTask"},
+                {"value":"商品类别粒度","index":"/trade/Multimodal-multigranularity/goodsgranularity"},
+                {"value":"时间粒度","index":"/trade/Multimodal-multigranularity/time_advise"},
+                {"value":"关联查询", "index":"/trade/relationAnalysis/relationQuery"},
+                {"value":"交易频次查询", "index":"/trade/relationAnalysis/transactionFrequency"},
+                {"value":"数据查询", "index":"/trade/dataFusion/dataquery"}
+              ]
             }
+        },
+        mounted() {
+          this.components = this.loadAll();
         }
     }
 </script>
