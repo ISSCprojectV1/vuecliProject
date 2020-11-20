825ecbdd34db3b1eb528529ecca147b9407ef496
RyanZephyr <cn_ryan@outlook.com>
2020-11-20 21:29:53 +0800

    将在线与离线模态下的表格视图、流程图视图、交易联盟图独立成组件。

---
+++ src/components/part3/Common/taskQueryFlowChart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    * 1.将鼠标悬空在任务节点上方，可显示详细任务信息 2.可拖动节点方便查看
+    <method1 ref="method1_child"></method1>
+  </div>
+</template>
+
+<script>
+import method1 from "@/components/part1/transactionProject/taskDictionary/method1";
+
+export default {
+  name: "taskQueryFlowChart",
+  components: {
+    method1
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
