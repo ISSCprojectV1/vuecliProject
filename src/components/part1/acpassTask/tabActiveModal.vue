fd3ae12398542dd1bd1b15e57babe33283e7d1dc
RyanZephyr <cn_ryan@outlook.com>
2021-03-13 11:31:03 +0800

    优化主动模态tab显示效果，统一界面风格。

--- src/components/part1/acpassTask/tabActiveModal.vue
+++ src/components/part1/acpassTask/tabActiveModal.vue
@@ -1,11 +1,12 @@
 <template>
   <div style="width: 100%">
-    <el-container style="width: 100%">
-      <el-aside style="width: 580px">
+    <el-container style="height: 700px; border: 10px solid #eee">
+      <el-aside width="750px" style="border: 10px solid #eee">
+        <h2>主动监管名单</h2>
         <el-table :data="dataTableActive" highlight-current-row @row-click="onClickTableActive">
-          <el-table-column prop="company" label="交易主体" width="120"></el-table-column>
-          <el-table-column prop="category" label="商品类型" width="80"></el-table-column>
-          <el-table-column prop="abnormalValue" label="异常值" width="70">
+          <el-table-column prop="company" label="交易主体" min-width="250"></el-table-column>
+          <el-table-column prop="category" label="商品类型" min-width="70"></el-table-column>
+          <el-table-column prop="abnormalValue" label="异常值" min-width="70">
             <template slot-scope="scope">
               <span v-if="Number.isNaN(scope.row.abnormalValue)">无</span>
               <span v-else-if="isAbnormal(scope.row.abnormalValue)" style="color: red">
@@ -16,19 +17,15 @@
                   </span>
             </template>
           </el-table-column>
-          <el-table-column label="信用等级" width="80">
+          <el-table-column label="信用等级" min-width="70">
             <template slot-scope="scope">
               <span v-if="Number.isNaN(scope.row.abnormalValue)"></span>
-              <span v-else-if="isAbnormal(scope.row.abnormalValue)">
-                    建议监管
-                  </span>
-              <span v-else>
-                    正常
-                  </span>
+              <span v-else-if="isAbnormal(scope.row.abnormalValue)">建议监管</span>
+              <span v-else>正常</span>
             </template>
           </el-table-column>
-          <el-table-column label="信用评分" width="80">80</el-table-column>
-          <el-table-column label="详细信息" fixed="right" width="130">
+          <el-table-column label="信用评分" min-width="70">80</el-table-column>
+          <el-table-column label="详细信息" fixed="right" min-width="70">
             <template slot-scope="scope">
               <el-button @click="goToDataQuery(scope.row.company)" type="text" size="small">实体统一</el-button>
             </template>
@@ -40,16 +37,17 @@
             background
             layout="prev, pager, next"
             @current-change="onPageChangeActive"
+            :page-size="8"
             :total="totalTableActive">
         </el-pagination>
       </el-aside>
-      <el-container style="width: 600px;">
-        <el-main style="padding-top: 0">
+      <el-container style="border: 10px solid #eee">
           <div id="tableCompanyDetail" style="width: 100%; height: 100%">
-            <el-table :data="dataTableDetail" style="width: 600px;">
-              <el-table-column prop="seller" label="主要交易卖方" min-width="150"></el-table-column>
+            <h2>主体交易详情</h2>
+            <el-table :data="dataTableDetail" style="width: 100%">
+              <el-table-column prop="seller" label="主要交易卖方" min-width="160"></el-table-column>
               <el-table-column prop="buyer" label="主要交易买方" min-width="150"></el-table-column>
-              <el-table-column prop="amount" label="交易数" min-width="40"></el-table-column>
+              <el-table-column prop="amount" label="交易数" min-width="50"></el-table-column>
             </el-table>
             <el-pagination
                 ref="pagination"
@@ -62,7 +60,6 @@
             >
             </el-pagination>
           </div>
-        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -87,21 +84,20 @@ export default {
   },
   created() {
     const id = this.$router.currentRoute.params.id;
-    this.queryActiveGroup(id, 1, 10);
+    this.queryActiveGroup(id, 1, 8);
   },
   mounted() {
     document.getElementById('tableCompanyDetail').style.display = 'none'
   },
   methods: {
     // table active group
-    queryActiveGroup(taskId, currentPage = 1, pageSize = 10) {
+    queryActiveGroup(taskId, currentPage = 1, pageSize = 8) {
       getActiveGroup(taskId, currentPage, pageSize).then(res => {
         this.dataTableActive = res.data.data.reslist;
         let counter = 1;
         this.dataTableActive.map((obj) => {
           this.$set(obj, 'id', counter++)
         });
-        console.log(this.dataTableActive)
         this.threshold = res.data.data.threshold;
         this.totalTableActive = res.data.data.total;
         // 顺便画初始图
@@ -113,7 +109,7 @@ export default {
     },
     onPageChangeActive(page) {
       const id = this.$router.currentRoute.params.id;
-      this.queryActiveGroup(id, page, 10);
+      this.queryActiveGroup(id, page, 8);
     },
     // 用于active table, 比较异常值与阈值并返回是否异常
     isAbnormal(abnormalValue) {
