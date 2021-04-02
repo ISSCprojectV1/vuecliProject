e6113a4bd8db2f007ec9b2e20344371edc3277ef
Ryan <cn_ryan@outlook.com>
2021-04-02 10:46:06 +0800

    style: code formatting.

--- src/components/admin/sideBar.vue
+++ src/components/admin/sideBar.vue
@@ -1,60 +1,51 @@
 <template>
-<div>
+  <div>
     <el-menu
-            :default-active="defaultActive"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
-            :default-openeds="['system']"
-            router>
-        <el-submenu index='system'>
-            <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>系统管理</span>
-            </template>
-            <el-menu-item index="/admin/user">
-                用户管理
-            </el-menu-item >
-            <el-menu-item index="/admin/role">
-                角色管理
-            </el-menu-item >
-            <el-menu-item index="/admin/menu">
-                菜单管理
-            </el-menu-item >
-            <el-menu-item index="/admin/auctionAdd">
-             新建拍卖
-            </el-menu-item >
-            <el-menu-item index="/admin/auctionNew">
-                拍卖管理
-            </el-menu-item >
-            <el-menu-item index="/admin/score">
-                积分管理
-            </el-menu-item >
-            <el-menu-item index="/admin/data">
-                数据管理
-            </el-menu-item >
-        </el-submenu>
+        :default-active="defaultActive"
+        class="el-menu-vertical-demo"
+        :default-openeds="['system']"
+        router>
+      <el-submenu index='system'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>系统管理</span>
+        </template>
+        <el-menu-item index="/admin/user">
+          用户管理
+        </el-menu-item>
+        <el-menu-item index="/admin/role">
+          角色管理
+        </el-menu-item>
+        <el-menu-item index="/admin/menu">
+          菜单管理
+        </el-menu-item>
+        <el-menu-item index="/admin/auctionAdd">
+          新建拍卖
+        </el-menu-item>
+        <el-menu-item index="/admin/auctionNew">
+          拍卖管理
+        </el-menu-item>
+        <el-menu-item index="/admin/score">
+          积分管理
+        </el-menu-item>
+        <el-menu-item index="/admin/data">
+          数据管理
+        </el-menu-item>
+      </el-submenu>
     </el-menu>
-</div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "sideBar",
-        methods: {
-            handleOpen(){
-                console.log("handleOpen")
-            },
-            handleClose(){
-                console.log("handleClose")
-            }
-        },
-        computed: {
-            defaultActive(){
-                return this.$route.path
-            }
-        }
+export default {
+  name: "sideBar",
+  methods: {},
+  computed: {
+    defaultActive() {
+      return this.$route.path
     }
+  }
+}
 </script>
 
 <style scoped lang="stylus">
--- src/router/admin/index.js
+++ src/router/admin/index.js
@@ -8,64 +8,38 @@ import score from "@/components/admin/score";
 import auctionAdd from "@/components/admin/auctionAdd";
 
 let routes = [{
-    path:'/admin',
-    component:Layout,
-    children:[
-        {
-            path:'/admin/user',
-            component:user
-        },
-        {
-            path:'/admin/auctionAdd',
-            component:auctionAdd
-        },
-        {
-            path:'/admin/role',
-            component: role
-        },
-        {
-            path:'/admin/menu',
-            component: menu
-        },
-        {
-            path:'/admin/auctionNew',
-            component: auction
-        },
-        {
-            path:'/admin/data',
-            component: data
-        },
-        {
-            path:'/admin/score',
-            component: score
-        }
-
-
-    ]
+  path: '/admin',
+  component: Layout,
+  children: [
+    {
+      path: '/admin/user',
+      component: user
+    },
+    {
+      path: '/admin/auctionAdd',
+      component: auctionAdd
+    },
+    {
+      path: '/admin/role',
+      component: role
+    },
+    {
+      path: '/admin/menu',
+      component: menu
+    },
+    {
+      path: '/admin/auctionNew',
+      component: auction
+    },
+    {
+      path: '/admin/data',
+      component: data
+    },
+    {
+      path: '/admin/score',
+      component: score
+    }
+  ]
 }]
 
 export default routes
-
-// {
-//     path:'/userinfo',
-//     component:Userinfo,
-//     children:[
-//         {
-//             path:'/',
-//             component:userindex
-//         },
-//         {
-//             path:'download',
-//             component:userdownload
-//         },
-//         {
-//             path:'score',
-//             component:userscore
-//         },
-//         {
-//             path:'group',
-//             component: usergroup
-//         }
-//
-//     ]
-// },
