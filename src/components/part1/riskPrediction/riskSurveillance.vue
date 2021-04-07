0eb73dc5fbdc63b1aa0f71cb85ec6d6bbe6a60b7
hantian123579@163.com <15l9JK15l9JK>
2021-04-07 16:36:11 +0800

    修改风险管理界面

--- src/components/part1/riskPrediction/riskSurveillance.vue
+++ src/components/part1/riskPrediction/riskSurveillance.vue
@@ -30,24 +30,7 @@
                 </template>
               </el-table-column>
               <el-table-column label="操作" min-width="80">
-                <template slot-scope="scope">
-                  <el-button type="text" class="el-option-in-table" @click="onClickModify(scope.row)">修改</el-button>
-                  <el-popconfirm
-                      confirm-button-text="确定"
-                      cancel-button-text="取消"
-                      title="是否确定送审？"
-                      @onConfirm="pendInfo(scope.row)">
-                    <el-button type="text" class="el-option-in-table" slot="reference">送审</el-button>
-                  </el-popconfirm>
-                  <el-button type="text" class="el-option-in-table" @click="goToRiskPage(scope.row)">监控</el-button>
-                  <el-popconfirm
-                      confirm-button-text="确定"
-                      cancel-button-text="取消"
-                      title="是否确定删除？"
-                      @onConfirm="deleteInfo(scope.row)">
-                    <el-button type="text" class="el-option-in-table" slot="reference">删除</el-button>
-                  </el-popconfirm>
-                </template>
+
               </el-table-column>
             </el-table>
           </el-tab-pane>
@@ -72,20 +55,14 @@
                 <template slot-scope="scope">
                   <el-button type="text" class="el-option-in-table" @click="onClickModify(scope.row)">修改</el-button>
                   <el-popconfirm
-                      confirm-button-text="确定"
-                      cancel-button-text="取消"
-                      title="是否确定送审？"
-                      @onConfirm="pendInfo(scope.row)">
+                          confirm-button-text="确定"
+                          cancel-button-text="取消"
+                          title="是否确定送审？"
+                          @onConfirm="pendInfo(scope.row)">
                     <el-button type="text" class="el-option-in-table" slot="reference">送审</el-button>
                   </el-popconfirm>
                   <el-button type="text" class="el-option-in-table" @click="goToRiskPage(scope.row)">监控</el-button>
-                  <el-popconfirm
-                      confirm-button-text="确定"
-                      cancel-button-text="取消"
-                      title="是否确定删除？"
-                      @onConfirm="deleteInfo(scope.row)">
-                    <el-button type="text" class="el-option-in-table" slot="reference">删除</el-button>
-                  </el-popconfirm>
+
                 </template>
               </el-table-column>
             </el-table>
@@ -133,26 +110,6 @@
             </el-table>
           </el-tab-pane>
 
-          <el-tab-pane label="已删除">
-            <el-table :data="formDeleted">
-              <el-table-column prop="id" label="序号" min-width="30"></el-table-column>
-              <el-table-column prop="goods" label="商品" min-width="50"></el-table-column>
-              <el-table-column prop="info" label="预警信息" min-width="40">
-                <template slot-scope="scope">
-                  <span v-if="scope.row.info === '高风险'" style="color: red">{{ scope.row.info }}</span>
-                  <span v-else-if="scope.row.info === '低风险'" style="color: green">{{ scope.row.info }}</span>
-                  <span v-else-if="scope.row.info === '中风险'" style="color: orange">{{ scope.row.info }}</span>
-                </template>
-              </el-table-column>
-              <el-table-column prop="time" label="发布时间" min-width="70">
-                <template slot-scope="scope">
-                  <span>{{ scope.row.time.split('.')[0].replace('T', ' ') }}</span>
-                </template>
-              </el-table-column>
-              <el-table-column label="操作" min-width="80">
-              </el-table-column>
-            </el-table>
-          </el-tab-pane>
 
         </el-tabs>
 
@@ -199,7 +156,48 @@
 <script>
 import * as echarts5 from "echarts5";
 import {getRiskInfoByStatus, updateRiskInfo} from "@/api/part1/riskPrediction";
+/*
+*  <template slot-scope="scope">
+                  <el-button type="text" class="el-option-in-table" @click="onClickModify(scope.row)">修改</el-button>
+                  <el-popconfirm
+                      confirm-button-text="确定"
+                      cancel-button-text="取消"
+                      title="是否确定送审？"
+                      @onConfirm="pendInfo(scope.row)">
+                    <el-button type="text" class="el-option-in-table" slot="reference">送审</el-button>
+                  </el-popconfirm>
+                  <el-button type="text" class="el-option-in-table" @click="goToRiskPage(scope.row)">监控</el-button>
+                  <el-popconfirm
+                      confirm-button-text="确定"
+                      cancel-button-text="取消"
+                      title="是否确定删除？"
+                      @onConfirm="deleteInfo(scope.row)">
+                    <el-button type="text" class="el-option-in-table" slot="reference">删除</el-button>
+                  </el-popconfirm>
+                </template>
+                *  <el-tab-pane label="已删除">
+            <el-table :data="formDeleted">
+              <el-table-column prop="id" label="序号" min-width="30"></el-table-column>
+              <el-table-column prop="goods" label="商品" min-width="50"></el-table-column>
+              <el-table-column prop="info" label="预警信息" min-width="40">
+                <template slot-scope="scope">
+                  <span v-if="scope.row.info === '高风险'" style="color: red">{{ scope.row.info }}</span>
+                  <span v-else-if="scope.row.info === '低风险'" style="color: green">{{ scope.row.info }}</span>
+                  <span v-else-if="scope.row.info === '中风险'" style="color: orange">{{ scope.row.info }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column prop="time" label="发布时间" min-width="70">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.time.split('.')[0].replace('T', ' ') }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column label="操作" min-width="80">
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
 
+*
+* */
 export default {
   name: "riskSurveillance",
   data() {
@@ -218,6 +216,7 @@ export default {
   },
   created() {
     this.getRiskInfo(0)
+
   },
   mounted() {
     this.drawChartRiskFrequency()
