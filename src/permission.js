b51a09d7b83f32663df411c8b62f33295717c56b
Ryan <cn_ryan@outlook.com>
2021-04-09 20:17:46 +0800

    style(src/permission.js): code formatting.

--- src/permission.js
+++ src/permission.js
@@ -2,48 +2,40 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import router from './router'
 import store from './store'
-import {setToken,getToken,removeToken} from "./utils/auth"
+import {setToken, getToken, removeToken} from "./utils/auth"
 
+const whiteList = ['/login', '/register',];
 
+router.beforeEach((to, from, next) => {
+  NProgress.start();
 
-
-const whiteList = ['/login','/register',];
-
-
-router.beforeEach((to,from,next)=>{
-    NProgress.start();
-
-    // console.log("addRoutes:"+store.state.permission.addRoutes)
-    if (whiteList.indexOf(to.path)!== -1){
-        //在免登陆白名单,直接进入
-        next();
-    }
-    else{
-        if (getToken()){
-            if (store.state.permission.addRoutes.length==0){
-                store.dispatch('GenerateRoutes',getToken()).then(/*accessRoutes=>{
+  if (whiteList.indexOf(to.path) !== -1) {
+    //在免登陆白名单,直接进入
+    next();
+  } else {
+    if (getToken()) {
+      if (store.state.permission.addRoutes.length === 0) {
+        store.dispatch('GenerateRoutes', getToken()).then(/*accessRoutes=>{
                     // console.log(router.options.routes.length);
                  //   router.addRoutes(accessRoutes);
                     next({...to,replace:true});
                     // console.log(router.options.routes.length);
-                }*/).catch(err=>{
-                    console.log(err);
-                    next({path:'/login'});
-                })
-            }
-            else {
-                next();
-                console.log(router.options.routes.length);
+                }*/).catch(err => {
+          console.log(err);
+          next({path: '/login'});
+        })
+      } else {
+        next();
+        console.log(router.options.routes.length);
 
-            }
-        }
-        else{
-            next('/login');
-        }
+      }
+    } else {
+      next('/login');
     }
+  }
 })
 
 router.afterEach(() => {
-    NProgress.done()
+  NProgress.done()
 })
 
