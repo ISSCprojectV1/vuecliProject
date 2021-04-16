95c332f34944dafa525139de4655210cba19749a
RyanZephyr <cn_ryan@outlook.com>
2021-04-16 20:40:37 +0800

    feature: 实现根据用户权限动态显示工作台菜单。

--- src/components/part1/common/leftNav.vue
+++ src/components/part1/common/leftNav.vue
@@ -10,38 +10,64 @@
             background-color="#00659B"
             text-color="#FFFFFF"
             active-text-color="#ffd04b" style="height: 100%">
-          <el-menu-item index="/trade/Dashboard" style="text-align: left;font-size: 20px">
+
+          <el-menu-item v-if="isAvailable('/trade/Dashboard')" index="/trade/Dashboard"
+                        style="text-align: left;font-size: 20px">
             <i class="el-icon-house"></i>
             <span slot="title">首页</span>
           </el-menu-item>
 
-          <el-submenu index="/trade/auction" style="text-align: left;">
+          <el-submenu v-if="isAvailable('/trade/auction') || isAvailable('/trade/uploadResource')"
+                      index="/trade/auction" style="text-align: left;">
             <template slot="title">
               <i class="el-icon-menu"></i>
               <span style="font-size: 20px">数据共享</span>
             </template>
-            <el-menu-item index="/trade/auction" style="text-align: left;padding-left: 80px;font-size: 18px">
+            <el-menu-item v-if="isAvailable('/trade/auction')" index="/trade/auction"
+                          style="text-align: left;padding-left: 80px;font-size: 18px">
               <span slot="title">拍卖中心</span>
             </el-menu-item>
-            <el-menu-item index="/trade/uploadResource" style="text-align: left;padding-left: 80px;font-size: 18px">
+            <el-menu-item v-if="isAvailable('/trade/uploadResource')" index="/trade/uploadResource"
+                          style="text-align: left;padding-left: 80px;font-size: 18px">
               <span slot="title">上传数据</span>
             </el-menu-item>
           </el-submenu>
+
           <el-submenu
-              v-for="routes in this.accessRoutes"
-              :key="routes.title"
-              :index="routes.title"
-              style="text-align: left">
+              v-if="isAvailable('/trade/Multimodal-multigranularity/stepBar') || isAvailable('/trade/Multimodal-multigranularity/goodsgranularity')"
+              index="/trade/Multimodal-multigranularity" style="text-align: left">
             <template slot="title">
               <i class="el-icon-menu"></i>
-              <span style="font-size: 20px">{{ routes.title }}</span>
+              <span style="font-size: 20px">模态粒度与建议</span>
             </template>
-            <el-menu-item :index="route.path" v-for="route in routes.routes" :key="route.path"
+            <el-menu-item v-if="isAvailable('/trade/Multimodal-multigranularity/stepBar')"
+                          index="/trade/Multimodal-multigranularity/stepBar"
                           style="text-align: left;padding-left: 80px;font-size: 18px">
-              <span slot="title">{{ route.meta.title }}</span>
+              <span slot="title">模态与粒度步骤</span>
+            </el-menu-item>
+            <el-menu-item v-if="isAvailable('/trade/Multimodal-multigranularity/goodsgranularity')"
+                          index="/trade/Multimodal-multigranularity/goodsgranularity"
+                          style="text-align: left;padding-left: 80px;font-size: 18px">
+              <span slot="title">时间与商品粒度</span>
             </el-menu-item>
           </el-submenu>
-          <el-menu-item index="/trade/riskPrediction/riskSurveillance/" style="text-align: left;font-size: 20px">
+          <!--          <el-submenu-->
+          <!--              v-for="routes in this.accessRoutes"-->
+          <!--              :key="routes.title"-->
+          <!--              :index="routes.title"-->
+          <!--              style="text-align: left">-->
+          <!--            <template slot="title">-->
+          <!--              <i class="el-icon-menu"></i>-->
+          <!--              <span style="font-size: 20px">{{ routes.title }}</span>-->
+          <!--            </template>-->
+          <!--            <el-menu-item :index="route.path" v-for="route in routes.routes" :key="route.path"-->
+          <!--                          style="text-align: left;padding-left: 80px;font-size: 18px">-->
+          <!--              <span slot="title">{{ route.meta.title }}</span>-->
+          <!--            </el-menu-item>-->
+          <!--          </el-submenu>-->
+
+          <el-menu-item v-if="isAvailable('/trade/riskPrediction/riskSurveillance')"
+                        index="/trade/riskPrediction/riskSurveillance/" style="text-align: left;font-size: 20px">
             <i class="el-icon-menu"></i>
             <span slot="title">风险预警监控</span>
           </el-menu-item>
@@ -89,23 +115,19 @@ export default {
   },
   created() {
   },
-  methods: {}
+  methods: {
+    // 判断当前用户是否有权限访问菜单对应界面，从而确定是否显示菜单
+    isAvailable(path) {
+      return this.$store.getters["permission/hasAccessTo"](path)
+    }
+  }
 }
 </script>
 <style>
-.el-submenu__title:hover {
-  background: #0677B3 !important;
-}
-
 .el-menu-vertical-demo {
   padding: 0;
   margin: 0;
   border: 0;
-}
-
-.el-icon-arrow-down:before {
-  content: "\e6df";
-  color: #ffffff;
 }
 
 .el-icon-menu:before {
@@ -117,5 +139,4 @@ export default {
   content: "\e754";
   color: #FFFFFF;
 }
-
 </style>
