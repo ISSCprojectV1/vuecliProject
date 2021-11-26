d7ddd0556b7626b1000ef8cfd7a35c889bb23623
taohansi <hans-tao@qq.com>
2021-11-26 11:01:48 +0800

    add keep-alive for tradingDetection component

--- src/components/part1/common/full.vue
+++ src/components/part1/common/full.vue
@@ -5,45 +5,49 @@
     </el-header>
 
     <el-container>
-      <el-aside style="width:250px;height: 100%">
+      <el-aside style="width: 250px; height: 100%">
         <leftNav></leftNav>
       </el-aside>
       <el-main>
-        <router-view></router-view>
+        <keep-alive include="tradingDetection">
+          <router-view></router-view>
+        </keep-alive>
       </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-
 import Header from "@/components/part3/Common/header";
 import leftNav from "@/components/part1/common/leftNav";
 
 export default {
   components: {
     Header,
-    leftNav
+    leftNav,
   },
   data() {
-    return {}
+    return {};
   },
-  methods: {}
-}
+  methods: {},
+};
 </script>
 
 <style lang="stylus" scoped>
-.el-header
-  //box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.5);
-  padding 0;
-  margin-bottom 1px
+.el-header {
+  // box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.5);
+  padding: 0;
+  margin-bottom: 1px;
+}
 
-.el-aside
-  //box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.5);
-  padding 0;
-  margin-bottom 1px
+.el-aside {
+  // box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.5);
+  padding: 0;
+  margin-bottom: 1px;
+}
 
-.el-main
-  padding 0;
-  background-color #ffffff;
+.el-main {
+  padding: 0;
+  background-color: #ffffff;
+}
 </style>
