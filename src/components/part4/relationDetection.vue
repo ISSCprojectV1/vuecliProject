1a3e20102a0c5527c78f829324c30b6ace770c22
hans <hans-tao@qq.com>
2022-06-21 15:28:22 +0800

    merge conflict

--- src/components/part4/relationDetection.vue
+++ src/components/part4/relationDetection.vue
@@ -49,7 +49,7 @@
         </div>
         <div
           id="container"
-          style="height: 700px; margin: 10px"
+          style="width: 925px; height: 700px; margin: 10px"
           ref="graph"
         ></div>
       </div>
