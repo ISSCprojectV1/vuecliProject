dcb8589216377b46addfd57a4ec312a6d62092a9
RyanZephyr <cn_ryan@outlook.com>
2023-03-20 22:38:48 +0800

    style: implement supervisionRec.

--- src/components/part4/DefaultRisk/supervisionRec.vue
+++ src/components/part4/DefaultRisk/supervisionRec.vue
@@ -1,28 +1,45 @@
 <template>
   <div>
     <!-- 参数设置 -->
-    <el-card class="box-card">
+    <el-card shadow="hover" class="box-card">
       <div slot="header" class="box-card-header">
         <span>监管推荐参数设置</span>
       </div>
-      <el-radio-group v-model="supervisionMode">
-        <el-radio-button label="global">全品类监管</el-radio-button>
-        <el-radio-button label="local">单品类监管</el-radio-button>
-      </el-radio-group>
 
-      <!-- 全品类监管参数 -->
-      <el-form v-if="supervisionMode === 'global'">
-        全品类
-      </el-form>
+      <!--      <el-radio-group v-model="supervisionMode">-->
+      <!--        <el-radio-button label="global">全品类监管</el-radio-button>-->
+      <!--        <el-radio-button label="local">单品类监管</el-radio-button>-->
+      <!--      </el-radio-group>-->
 
-      <!-- 单品类监管参数 -->
-      <el-form v-if="supervisionMode === 'local'">
-        单品类
+      <el-form :model="form" label-width="auto" label-position="top" :inline="true">
+
+        <el-form-item label="监管模式">
+          <el-radio-group v-model="form.supervisionMode">
+            <el-radio-button label="global">全品类监管</el-radio-button>
+            <el-radio-button label="local">单品类监管</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="目标交易品类" v-show="form.supervisionMode==='local'">
+          <el-select v-model="form.targetCategory" placehoder="请选择">
+
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="交易主体数量">
+          <el-input-number v-model="form.k" :min="1"></el-input-number>
+        </el-form-item>
+
+        <el-form-item label="操作">
+          <el-button type="primary">查询监管推荐</el-button>
+
+        </el-form-item>
+
       </el-form>
 
     </el-card>
 
-    <el-card class="box-card">
+    <el-card shadow="hover" class="box-card">
       <div slot="header" class="box-card-header">
         <span>监管推荐详细信息</span>
       </div>
@@ -35,7 +52,11 @@ export default {
   name: "supervisionGRec",
   data() {
     return {
-      supervisionMode: 'global',
+      form: {
+        supervisionMode: 'global',
+        targetCategory: '',
+        k: 1,
+      },
     }
   },
 }
@@ -56,16 +77,16 @@ export default {
   padding: 0
 }
 
-/deep/ .el-radio-button__inner {
-  font-weight: bold;
-  font-size: initial;
-}
+/*/deep/ .el-radio-button__inner {*/
+/*  font-weight: bold;*/
+/*  font-size: initial;*/
+/*}*/
 
-/deep/ .el-radio-button .el-radio-button__inner {
-  color: black;
-}
+/*/deep/ .el-radio-button .el-radio-button__inner {*/
+/*  color: black;*/
+/*}*/
 
-/deep/ .el-radio-button.is-active .el-radio-button__inner {
-  color: white;
-}
+/*/deep/ .el-radio-button.is-active .el-radio-button__inner {*/
+/*  color: white;*/
+/*}*/
 </style>
